3cb77fb2256275cda9c519030d98b44956263ba3
Jeancarlos Carvajal <jeancarloscarvajalavila@gmail.com>
2022-05-28 13:24:45 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+        
+
+	// Función aplica sólo a Cadenas de Texto (String)
+        function console_log(datos){
+            // Creamos los estilos del Console log personalizado
+            var css = 'font-size: 25px; background: #03a9f4; color: white; font-weight: bold; padding: 5px 15px;';
+            // Agregamos los datos y el css al console log
+            console.log('%c' + datos, css);
+        }
+
+        // Probando nuestro Console log personalizado
+        console_log('HOLA MUNDO');
