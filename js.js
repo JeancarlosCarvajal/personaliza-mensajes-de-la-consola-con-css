20ab9abd876908541a0791360d5e7111dbaecc0b
Jeancarlos Carvajal <jeancarloscarvajalavila@gmail.com>
2022-05-28 13:28:35 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
         
 
-	// Función aplica sólo a Cadenas de Texto (String)
+        // Función aplica sólo a Cadenas de Texto (String)
         function console_log(datos){
             // Creamos los estilos del Console log personalizado
-            var css = 'font-size: 25px; background: #03a9f4; color: white; font-weight: bold; padding: 5px 15px;';
-            // Agregamos los datos y el css al console log
-            console.log('%c' + datos, css);
+            var estilos = 'font-size: 25px; background: #03a9f4; color: white; font-weight: bold; padding: 5px 15px;';
+            // Agregamos los datos y el estilos al console log
+            console.log('%c' + datos, estilos);
         }
 
         // Probando nuestro Console log personalizado
